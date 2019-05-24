4f5d2d1c564307b4e9ef4b596cea012b0f3f442e
myungho <skqoaudgh2@hanmail.net>
2019-05-24 23:24:08 +0900

    Backdrop, Modal, Spinner 컴포넌트 추가

---
+++ frontend/src/components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Spinner.css';
+
+const spinner = props => {
+    <div className="spinner"><div className="lds-dual-ring"></div></div>
+}
+
+export default spinner;
